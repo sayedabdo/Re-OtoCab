d4c115e71549391fe20c03913172b20164f6cb96
sayedabdo <sayed.m.abdo123@gmail.com>
2020-06-04 02:40:55 +0200

    draw route :)

--- Re-OtoCab/ViewController.swift
+++ Re-OtoCab/ViewController.swift
@@ -24,7 +24,13 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
     // A default location to use when location permission is not granted.
     let defaultLocation = CLLocation(latitude: -33.869405, longitude: 151.199)
     
-    
+    // MARK: Define the source latitude and longitude
+       var sourceLat : Double = 0.0
+       var sourceLng : Double = 0.0
+    // MARK: Define the destination latitude and longitude
+       var destinationLat : Double = 0.0
+       var destinationLng : Double = 0.0
+       var selectedDestination : Bool = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -48,7 +54,25 @@ class ViewController: UIViewController, CLLocationManagerDelegate, GMSMapViewDel
        mapView.delegate = self
     }
 
+    @IBAction func showDirection(_ sender: Any) {
+        if selectedDestination == true{
+           // drawRoute(sourceLat: sourceLat, sourceLng: sourceLng, destinationLat: destinationLat, destinationLng: destinationLng)
+          //  let alert = UIAlertController(title: "urgent !!😱😱", message: "Your Soure Location is \(sourceLat) , \(sourceLng).\n Your destination Location is \(destinationLat) , \(destinationLng).\n but , You must  Billing on the Google to drawRoute 😉", preferredStyle: .alert)
+         //   alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+         //   }))
 
+         //   self.present(alert, animated: true, completion: nil)
+            drawPath()
+        }else{
+            let alert = UIAlertController(title: "Sorry !!😱😱", message: "please🙏 , select your distination 😉", preferredStyle: .alert)
+            alert.addAction(UIAlertAction(title: "OK", style: .default, handler: { action in
+            }))
+
+            self.present(alert, animated: true, completion: nil)
+        }
+                drawPath()
+    }
+    
 }
 // get current location
 extension ViewController{
@@ -61,8 +85,8 @@ extension ViewController{
       let camera = GMSCameraPosition.camera(withLatitude: location.coordinate.latitude,
                                             longitude: location.coordinate.longitude,
                                             zoom: zoomLevel)
-      //sourceLat = location.coordinate.latitude
-      //sourceLng = location.coordinate.longitude
+      sourceLat = location.coordinate.latitude
+      sourceLng = location.coordinate.longitude
       if mapView.isHidden {
         mapView.isHidden = false
         mapView.camera = camera
@@ -100,17 +124,45 @@ extension ViewController{
     func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D){
        print("You tapped at \(coordinate.latitude), \(coordinate.longitude)")
       // destination = "\(coordinate.latitude),\(coordinate.longitude)"
-     //  destinationLat =  coordinate.latitude
-     //  destinationLng =  coordinate.longitude
+       destinationLat =  coordinate.latitude
+       destinationLng =  coordinate.longitude
        mapView.clear() // clearing Pin before adding new
        let marker = GMSMarker(position: coordinate)
        marker.title = "Your Destination"
        marker.map = mapView
-      // selectedDestination = true
+       selectedDestination = true
     }
 }
 // Ddraw Route
 extension ViewController{
+    func drawPath() {
+        let origin = "\(sourceLat),\(sourceLng)"
+        let destination = "\(destinationLat),\(destinationLng)"
+       let url = "https://maps.googleapis.com/maps/api/directions/json?origin=\(origin)&destination=\(destination)&mode=driving"
+       AF.request(url).responseJSON { (reseponse) in
+                    guard let data = reseponse.data else {
+                        return
+                    }
+                    do {
+                        let jsonData = try JSON(data: data)
+                        print(jsonData)
+                        let routes = jsonData["routes"].arrayValue
+
+                        for route in routes {
+                            let overview_polyline = route["overview_polyline"].dictionary
+                            let points = overview_polyline?["points"]?.string
+                            let path = GMSPath.init(fromEncodedPath: points ?? "")
+                            let polyline = GMSPolyline.init(path: path)
+                            polyline.strokeColor = .systemBlue
+                            polyline.strokeWidth = 5
+                            polyline.map = self.mapView
+                        }
+                    }
+                     catch let error {
+                        print(error.localizedDescription)
+                    }
+            }
+    }
     
 }
 // search on place
